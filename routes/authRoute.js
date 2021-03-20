b57710c8f6fd4a63e219f63ae68ee71e2426c060
Miss J Hayek <jenniferanne.hayek@gmail.com>
2021-03-19 21:18:06 -0700

    first commit

---
+++ routes/authRoute.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const passport = require("../middleware/passport");
+const { forwardAuthenticated } = require("../middleware/checkAuth");
+
+const router = express.Router();
+
+router.get("/login", forwardAuthenticated, (req, res) => res.render("login"));
+
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/dashboard",
+    failureRedirect: "/auth/login",
+  })
+);
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/auth/login");
+});
+
+//added for git login
+router.get('/github',
+  passport.authenticate('github', { scope: ['user:email'] }));
+
+router.get('/github/callback',
+  passport.authenticate('github', { failureRedirect: '/login' }),
+  function (req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/');
+  });
+//added for git login
+
+
+module.exports = router;
